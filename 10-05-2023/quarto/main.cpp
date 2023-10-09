63eb85c81a7c56762f4ae9ef881338a8834c2154
Matthew Gasparetti <106164674+b3nj4m1no@users.noreply.github.com>
2023-10-09 16:30:24 +0200

    Update main.cpp

--- 10-05-2023/quarto/main.cpp
+++ 10-05-2023/quarto/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <windows.h>    // Solamente per avere l'output colorato
 using namespace std;
 
 // Calcolare il prezzo di un articolo conoscendo il prezzo base,
@@ -7,10 +6,6 @@ using namespace std;
 
 int main() {
     float prezzoBase, prezzoScontato, prezzoFinale, iva, sconto;
-
-    // 0 per lo sfondo (Nero)
-    // A per il colore del testo (Verde)
-    system("Color 0A"); 
 
     cout << "Inserisci il prezzo base: ";
     cin >> prezzoBase;
